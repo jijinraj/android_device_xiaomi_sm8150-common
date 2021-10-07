1640a05f54c800087056c91935868df532547162
KostyaJRZ <kostyajrzaws@gmail.com>
2021-10-07 13:30:38 +0300

    sm8150-common : Disable WfdCommon

--- msmnile.mk
+++ msmnile.mk
@@ -428,8 +428,5 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8150-common/sm8150-common-vendor.mk)
